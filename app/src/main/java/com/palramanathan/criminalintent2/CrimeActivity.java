9118e845237660473f4f1f4f037bbe3a2a2ed863
palramanathan <rpalaniappan@gmail.com>
2014-11-26 11:40:26 -0500

    Fixes

--- app/src/main/java/com/palramanathan/criminalintent2/CrimeActivity.java
+++ app/src/main/java/com/palramanathan/criminalintent2/CrimeActivity.java
@@ -1,6 +1,8 @@
 package com.palramanathan.criminalintent2;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -11,16 +13,16 @@ public class CrimeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
-//        FragmentManager fragmentManager = getFragmentManager();
-//
-//        Fragment fragment = fragmentManager.findFragmentById(R.id.fragmentContainer);
-//
-//        if (fragment == null) {
-//            fragment = new CrimeFragment();
-//            fragmentManager.beginTransaction()
-//                    .add(R.id.fragmentContainer, fragment)
-//                    .commit();
-//        }
+        FragmentManager fragmentManager = getFragmentManager();
+
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
     }
 
     @Override
